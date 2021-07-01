980370c12fd33e5016e9aa8f8c5b45e1d48e0f0d
KiwookKim <rokaf1771@gmail.com>
2021-07-02 04:17:22 +0900

    error catch
    Nice!

--- Background Charger/hello.js
+++ Background Charger/hello.js
@@ -1,5 +1,5 @@
 function hexGenerator() {
-        const hexNumbers = [
+    const hexNumbers = [
         0,
         1,
         2,
@@ -18,18 +18,18 @@ function hexGenerator() {
         'F'
     ]
     let result = "#"
-    for(let i=0; i<6; i++){
-        const randomIndex = Math.floor(Math.random()*hexNumbers.length);
-        result +=  hexNumbers[randomIndex];
+    for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * hexNumbers.length);
+        result += hexNumbers[randomIndex];
     }
-    
+
     return result;
 }
 
 
 
-const $span= document.querySelector(".hexcolor");
-const $button= document.querySelector(".btn");
+const $span = document.querySelector(".hexcolor");
+const $button = document.querySelector(".btn");
 const $body = document.querySelector("body")
 let colorValue = $span.innerText = hexGenerator();
 
@@ -39,7 +39,8 @@ function paintBg(e) {
     e.preventDefault();
 
     $span.innerText = hexGenerator();
-    $body.style.backgroundColor = hexGenerator();
+    let colorValue = $span.innerText = hexGenerator();
+    $body.style.backgroundColor = colorValue;
     // span 값과 body의 background-color 값이 서로 다른 hexGenerator()로 생성됨....... 어떻게 값을 일치시킬것인가?
 }
 
